816ee50b51ac055a523b31398c4dba77566fbb0a
GHOST-Aram <owinoojala@gmail.com>
2024-02-18 14:42:16 +0300

    chore: Write test for users Update Controller method: Non existing User
          - Expect response with status code 201
          - Expect response with the updated document url in the location property
          - Expect response with JSON content type

--- src/app/users/controller/tests/put.test.ts
+++ src/app/users/controller/tests/put.test.ts
@@ -1,6 +1,6 @@
 import { controller } from './test.config';
 import { describe, test, expect } from '@jest/globals'
-import { EXISTING_USER_ID, userData } from "./test.data";
+import { EXISTING_USER_ID,NOT_EXIST_USER_ID, userData } from "./test.data";
 
 describe('Update One Controller', () =>{
 
@@ -9,6 +9,16 @@ describe('Update One Controller', () =>{
             const response = await controller.updateOne(EXISTING_USER_ID, userData)
             
             expect(response.status).toEqual(200)
+            expect(response.headers.get('Content-Type')).toMatch(/json/)
+            expect(response.headers.get('Location')).toMatch(/\/users\/[a-f0-9]/i)
+        }
+    )
+
+    test('Responds with created resource url, status 201: Target document not found', 
+        async() => {
+            const response = await controller.updateOne(NOT_EXIST_USER_ID, userData)
+            
+            expect(response.status).toEqual(201)
             expect(response.headers.get('Content-Type')).toMatch(/json/)
             expect(response.headers.get('Location')).toMatch(/\/users\/[a-f0-9]/i)
         }
