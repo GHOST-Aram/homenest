6d11cae849b0712fc03fab1f3f629d5db1f03e28
GHOST-Aram <owinoojala@gmail.com>
2024-02-18 13:52:59 +0300

    feat: Write tests for GET many users controller method
          - Write tests for Controller.getMany method
          - Expect status 200
          - Expect JSON response
          - Expect paginated data
          - Expect data of array type

--- src/app/users/controller/get.test.ts
+++ src/app/users/controller/get.test.ts
@@ -1,7 +1,7 @@
 import { controller } from './test.config';
 import { describe, test, expect } from '@jest/globals'
 
-describe('GET route handler (GET One)', () =>{
+describe('GET route handler', () =>{
     test('Responds with Not found, status 404: target user not found',
         async() => {
             const response = await controller.getOne('64c9e4f2df7cc072af2ac9e8')
@@ -21,5 +21,15 @@ describe('GET route handler (GET One)', () =>{
             expect(await response.json()).toBeTruthy()
         }
     )
-}
-)
+
+    test('Responds with paginated items, status 200: GET many', 
+        async() =>{
+            const response = await controller.getMany({ skip: 0, limit: 20})
+            const data = await response.json()
+
+            expect(response.status).toEqual(200)
+            expect(response.headers.get('Content-Type')).toMatch(/json/)
+            expect(Array.isArray(data)).toBeTruthy()
+        }
+    )
+})
