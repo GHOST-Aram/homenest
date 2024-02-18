00f407dd75860c2e5c4923f03b5020b663ed703f
GHOST-Aram <owinoojala@gmail.com>
2024-02-18 13:18:41 +0300

    chore: Write test for successful GET request handler
           - Expect status 200
           - Expect response.json to be truthy
           - Expect JSON content

--- src/app/users/controller/get.test.ts
+++ src/app/users/controller/get.test.ts
@@ -10,5 +10,16 @@ describe('GET route handler (GET One)', () =>{
             expect(response.headers.get('Content-Type')).toMatch(/json/)
         }
     )
+
+    test('Responds with found item, status 200: Document found', 
+   
+        async() =>{
+            const response = await controller.getOne('64c9e4f2df7cc072af2ac9e4')
+
+            expect(response.status).toEqual(200)
+            expect(response.headers.get('Content-Type')).toMatch(/json/)
+            expect(await response.json()).toBeTruthy()
+        }
+    )
 }
 )
