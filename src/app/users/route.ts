668343a8693b0051103c809b9804033ede429fbf
GHOST-Aram <owinoojala@gmail.com>
2024-02-19 09:27:13 +0300

    chore: Alter directory structure
         - Remove users/api folder and move its content to user directory

--- src/app/users/route.ts
+++ src/app/users/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest } from "next/server";
-import { DataAccess } from "../data-access/data-access";
-import { User } from "../data-access/model";
-import { Controller, Paginator } from "../controller/controller";
-import '../config/db'
+import { DataAccess } from "./data-access/data-access";
+import { User } from "./data-access/model";
+import { Controller, Paginator } from "./controller/controller";
+import './config/db'
 import { Validator } from "@/z-library/validation/validator";
 import { userSchema } from "./validation-schema";
 import { handleServerErrors } from "@/z-library/HTTP/http-errors";
