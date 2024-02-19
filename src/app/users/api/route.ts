63b76e3362a5ba90d60269e1517eae514c5ad5f9
GHOST-Aram <owinoojala@gmail.com>
2024-02-19 08:35:27 +0300

    feat: Implement Pagination in GET route handler
         - Extract page and pagination limit values from request search params
         - Create a paginator Object
         - Calculate the number of documents to be skipped
         - Supply paginator object to the controller method as argument

--- src/app/users/api/route.ts
+++ src/app/users/api/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from "next/server";
 import { DataAccess } from "../data-access/data-access";
 import { User } from "../data-access/model";
-import { Controller } from "../controller/controller";
+import { Controller, Paginator } from "../controller/controller";
 import '../config/db'
 import { Validator } from "@/z-library/validation/validator";
 import { userSchema } from "./validation-schema";
@@ -19,9 +19,29 @@ export const POST = async(request: NextRequest) =>{
         return validator.handleValidationErrors(validationErrors)
     } else {
         try{
-            return controller.addNew(userData)
+            return await controller.addNew(userData)
         } catch(error){
-            return handleServerErrors(error)
+            return handleServerErrors()
         }
+    }
+}
+
+export const GET = async(request: NextRequest) =>{
+    const page = request.nextUrl.searchParams.get("page")
+    const limit = request.nextUrl.searchParams.get("limit")
+
+    const paginationLimit = Number(limit)
+    const skipDocsNumber = (Number(page) - 1) * paginationLimit
+
+    const paginator: Paginator = {
+        limit: paginationLimit ? paginationLimit: 10,
+        skip: skipDocsNumber ? skipDocsNumber : 0
+    }
+
+    
+    try {
+        return await controller.getMany(paginator)
+    } catch (error) {
+        return handleServerErrors()
     }
 }
