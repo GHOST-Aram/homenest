2e31f8b83df7841e3dd59f419c4dce39e6fcf6fb
GHOST-Aram <owinoojala@gmail.com>
2024-02-19 21:57:26 +0300

    feat: Handle button click event
         - Send POST request to create new user on button click

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,13 +2,37 @@
 import React, { ChangeEvent, useState } from 'react'
 import TextField from '@mui/material/TextField'
 import Paper from '@mui/material/Paper'
-import { Box, Button } from '@mui/material'
+import { 
+    Box, 
+    Button, 
+    FormControl, 
+    FormControlLabel, 
+    FormLabel, 
+    Radio, 
+    RadioGroup 
+} from '@mui/material'
 
-
-const SignUpForm = () => {
-    const [formData, setFormData] = useState({
-        fullName:'', email: '', password1: '', password2: ''
+const createUser = async(data: Object) =>{
+    const response = await fetch('/users/api', {
+        'method': 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
     })
+
+    return response
+}
+const SignUpForm = () => {
+    const [status, setStatus] = useState('idle')
+    const [formData, setFormData] = useState({
+        fullName:'', 
+        email: '', 
+        password1: '', 
+        password2: '', 
+        role:'tenant'
+    })
+
 
     const collectFormData = (event: ChangeEvent<HTMLInputElement>) =>{
         const { name, value } = event.target
@@ -19,46 +43,83 @@ const SignUpForm = () => {
         })
     }
 
-  return (
+    return (
         <main className='py-16 px-8'>
             <form action="" >
                 <Paper elevation={8} className='p-8 w-full space-y-4 md:w-4/5 lg:w-3/5 m-auto'>
                     <h1 className='text-center text-blue-600 font-md text-2xl'>
                         Welcome to Homenest
                     </h1>
+                    
                    
                     <Box className='w-full flex flex-col lg:flex-row gap-4 justify-space-between'>
-                        <TextField placeholder='Full name' sx={{width: '100%'}} 
-                            required type='text' 
-                            value={formData.fullName} 
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => { collectFormData(e) }} 
-                            name = {'fullName'}
-                        />
-                        <TextField placeholder='Email' sx={{width: '100%'}}
-                            required type='email' 
-                            value={formData.email} 
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => collectFormData(e)} 
-                            name ='email'
-                        />
+                        <FormControl fullWidth>
+                            <TextField placeholder='Full name' sx={{width: '100%'}} 
+                                required type='text' 
+                                value={formData.fullName} 
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => { collectFormData(e) }} 
+                                name = {'fullName'}
+                            />
+                        </FormControl>
+                        <FormControl fullWidth>
+                            <TextField placeholder='Email' sx={{width: '100%'}}
+                                required type='email' 
+                                value={formData.email} 
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => collectFormData(e)} 
+                                name ='email'
+                            />
+                        </FormControl>
                     </Box>
                     <Box className='w-full flex flex-col lg:flex-row gap-4 justify-space-between'>
-                        <TextField placeholder='Password' sx={{width: '100%'}}
-                            required type='password' 
-                            value={formData.password1} 
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => collectFormData(e)} 
-                            name='password1'
-                        />
-                        <TextField placeholder='Confirm Password' sx={{width: '100%'}}
-                            required type='password' 
-                            value={formData.password2} 
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => collectFormData(e)} 
-                            name='password2'
-                        />
+                        <FormControl fullWidth>
+                            <TextField placeholder='Password' sx={{width: '100%'}}
+                                required type='password' 
+                                value={formData.password1} 
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => collectFormData(e)} 
+                                name='password1'
+                            />
+                        </FormControl>
+                        <FormControl fullWidth>
+                            <TextField placeholder='Confirm Password' sx={{width: '100%'}}
+                                required type='password' 
+                                value={formData.password2} 
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => collectFormData(e)} 
+                                name='password2'
+                            />
+                        </FormControl>
                     </Box>
+                    <FormControl fullWidth>
+                        <FormLabel>Sign Up as</FormLabel>
+                        <RadioGroup 
+                            defaultValue={'tenant'} 
+                            value={formData.role} 
+                            name='role' 
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => collectFormData(e)}
+                            className='flex flex-row'
+                        >
+                            <FormControlLabel value={'tenant'} control={<Radio />} label={'Tenant'}/>
+                            <FormControlLabel value={'landlord'} control={<Radio />} label={'Landlord'}/>
+                        </RadioGroup>
+                    </FormControl>
+                    
                     <Button 
                         className='w-full bg-blue-700 text-white'
                         variant='contained' 
-                        onClick={()=> console.log(formData)}
+                        onClick={async()=> {
+                            setStatus('loading')
+                            console.log('started')
+                            const response = await createUser({
+                                name: formData.fullName,
+                                email: formData.email,
+                                password: formData.password1,
+                                role: formData.role
+                            })
+                            const location = response.headers.get('Location')
+                            console.log(await response.json())
+                            console.log(location)
+                            console.log(response.status)
+                            setStatus('done')
+                        }}
                     >
                         Sign Up
                     </Button>
